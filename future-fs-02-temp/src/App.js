8a92c65051b79920561343579c0d8f4fb45340b8
Stephen Ampofo <skampofo78@gmail.com>
2025-04-01 12:44:41 +0000

    Initial setup with pure CSS

---
+++ future-fs-02-temp/src/App.js
@@ -0,0 +1,13 @@
+import './index.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Weather Dashboard</h1>
+      {/* Components will go here */}
+    </div>
+  );
+}
+
+export default App;
